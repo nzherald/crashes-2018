d3beb1b979bbf76154d3846816d9295689d402c0
Chris Knox <chris.knox@nzherald.co.nz>
2018-12-18 06:10:48 +1300

    pull in scrollama setup - will fine tune - and remove d3 ...

--- interactive/src/Main.elm
+++ interactive/src/Main.elm
@@ -1,24 +1,24 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, text, section, p)
+import Html.Attributes exposing (src, attribute, id, class)
 import Http
 
-import DataTypes exposing (..)
 
 
+-- import DataTypes exposing (..)
 ---- MODEL ----
 
 
 type alias Model =
-    { dummy : Maybe Foo
+    { dummy : Maybe Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model Nothing, getData )
+    ( Model Nothing, Cmd.none )
 
 
 
@@ -26,15 +26,17 @@ init =
 
 
 type Msg
-    = DataLoad (Result Http.Error Foo)
+    = DataLoad (Result Http.Error Int)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        DataLoad (Err e) -> ( model, Cmd.none )
-        DataLoad (Ok f) -> ({ model | dummy = Just f }, Cmd.none)
-    
+        DataLoad (Err e) ->
+            ( model, Cmd.none )
+
+        DataLoad (Ok f) ->
+            ( { model | dummy = Just f }, Cmd.none )
 
 
 
@@ -43,20 +45,28 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
-        , div [] [ text <| 
-            case model.dummy of 
-                    Nothing -> ""
-                    Just d -> "Hello " ++ d.f_name
-        ]
+    section [ id "scroll" ]
+        [ div [ class "scroll__graphic sticky" ]
+            [ div [ class "chart" ]
+                [ p [] [ text "0" ]
+                ]
+            ]
+        , div [ class "scroll__text" ]
+            [ div [ class "step", attribute "data-step" "1" ]
+                [ p [] [ text "STEP 1" ]
+                ]
+            , div [ class "step", attribute "data-step" "2" ]
+                [ p [] [ text "STEP 2" ]
+                ]
+            , div [ class "step", attribute "data-step" "3" ]
+                [ p [] [ text "STEP 3" ]
+                ]
+            , div [ class "step", attribute "data-step" "4" ]
+                [ p [] [ text "STEP 4" ]
+                ]
+            ]
         ]
 
-
-getData : Cmd Msg
-getData =
-  Http.send DataLoad (Http.get "play.json" jsonDecFoo)
 
 
 ---- PROGRAM ----
--- interactive/src/root.js
+++ interactive/src/root.js
@@ -1,7 +1,86 @@
+import Stickyfill from 'stickyfilljs'
+import scrollama from 'scrollama'
+
+
 import "./base.less"
 import "./root.less"
 import {Elm} from "./Main.elm"
 
+const d3 = window.d3;
+
+function initScrollama() {
+    // using d3 for convenience
+		var container = d3.select('#scroll');
+		var graphic = container.select('.scroll__graphic');
+		var chart = graphic.select('.chart');
+		var text = container.select('.scroll__text');
+		var step = text.selectAll('.step');
+		// initialize the scrollama
+		var scroller = scrollama();
+		// generic window resize listener event
+		function handleResize() {
+			// 1. update height of step elements
+			var stepHeight = Math.floor(window.innerHeight * 0.75);
+			step.style('height', stepHeight + 'px');
+			// 2. update width/height of graphic element
+			var bodyWidth = d3.select('body').node().offsetWidth;
+			var textWidth = text.node().offsetWidth;
+			var graphicWidth = bodyWidth - textWidth;
+			graphic
+				.style('width', graphicWidth + 'px')
+				.style('height', window.innerHeight + 'px');
+			var chartMargin = 32;
+			var chartWidth = graphic.node().offsetWidth - chartMargin;
+			chart
+				.style('width', chartWidth + 'px')
+				.style('height', Math.floor(window.innerHeight / 2) + 'px');
+			// 3. tell scrollama to update new element dimensions
+			scroller.resize();
+		}
+		// scrollama event handlers
+		function handleStepEnter(response) {
+			// response = { element, direction, index }
+			// add color to current step only
+			step.classed('is-active', function (d, i) {
+				return i === response.index;
+			})
+			// update graphic based on step
+			chart.select('p').text(response.index + 1)
+		}
+		function handleContainerEnter(response) {
+			// response = { direction }
+		}
+		function handleContainerExit(response) {
+			// response = { direction }
+		}
+		function setupStickyfill() {
+			d3.selectAll('.sticky').each(function () {
+				Stickyfill.add(this);
+			});
+		}
+		function init() {
+			setupStickyfill();
+			// 1. force a resize on load to ensure proper dimensions are sent to scrollama
+			handleResize();
+			// 2. setup the scroller passing options
+			// this will also initialize trigger observations
+			// 3. bind scrollama event handlers (this can be chained like below)
+			scroller.setup({
+				container: '#scroll',
+				graphic: '.scroll__graphic',
+				text: '.scroll__text',
+				step: '.scroll__text .step',
+				debug: true,
+			})
+				.onStepEnter(handleStepEnter)
+				.onContainerEnter(handleContainerEnter)
+				.onContainerExit(handleContainerExit);
+			// setup resize event
+			window.addEventListener('resize', handleResize);
+		}
+		// kick things off
+		init();
+}
 
 class Main {
     constructor () {
@@ -10,6 +89,7 @@ class Main {
             node: document.getElementById('elm')
           });
         this.fadeOut()
+        initScrollama()
     }
 
     fadeOut (b) {
